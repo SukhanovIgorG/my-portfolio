43abd79bcd14a7a3efe0ffb3a6a49068c03140d1
SukhanovIgorG <sukhanovgarik@yandex.ru>
2024-05-19 21:06:18 +0300

    style/add logo image and color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom';
+import logo from "../../assets/develoceraptor.png";
+
 import styles from './header.module.sass';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <Link to='/' className={styles.logo}>
+        <span>WEB.</span>
+        <img className={styles.image} src={logo}></img>
+        <br />
+        <span>DEVeloceraptor</span>
+      </Link>
       <div>
       <button>Home</button>
       <button>Blog</button>
       <button>Contact</button>
       </div>
-      <Link to='/' className={styles.logo}>
-        <span>Sukhanov.</span>
-        <br />
-        <span>Web</span>
-      </Link>
       <div>
         <button>AI</button>
         <button>login</button>
